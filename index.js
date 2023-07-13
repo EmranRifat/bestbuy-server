984538853078c388e7f193012cc16a240568c5e9
EmranRifat <imran15-1919@diu.edu.bd>
2023-07-14 00:30:13 +0600

    jwt

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
  // pass: KpFiSlyjfQU4MV3s
 
 const categories=require("./data/Catagories.json"); 
-const req = require('express/lib/request');
+// const req = require('express/lib/request');
 
 
 // Database Connection
@@ -25,6 +25,29 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // console.log(uri);
 
 const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1,strict: true,       deprecationErrors: true,}});
+
+    // varify jwt function //
+
+    function verifyJWT(req, res, next) {
+      // console.log(req.headers.authorization);
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+          return res.status(401).send('unauthorized access');
+      }
+      const token = authHeader.split(' ')[1];
+  
+      jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+          if (err) {
+              return res.status(403).send({ message: 'forbidden access' })
+          }
+          req.decoded = decoded;
+          next();
+      })
+  
+  }
+
+
+
 
 async function run() {
 
@@ -65,14 +88,14 @@ async function run() {
 
 
     // get user specific orders //
-  app.get('/order',async(req,res)=>{
-    // let query={};
-    // if(req.query.email){
-    //   query={
-    //     email:req.query.email
-    //   }
-    // }
+  app.get('/order',verifyJWT,async(req,res)=>{
+
     const email=req.query.email;
+    const decodedEmail=req.decoded.email;  
+    // console.log('inside',decodedEmail);
+    if(email!==decodedEmail){
+      return res.status(403).send({message:'forbidden access '})
+    }
     const query={email:email};
     const order=await ordersCollection.find(query).toArray();
     res.send(order);
@@ -89,14 +112,53 @@ async function run() {
     });
 
     // save user in DB //
-    app.post('user',async(req,res)=>{
+    app.post('/users',async(req,res)=>{
       const user=req.body;
       const result=await usersCollection.insertOne(user);
       res.send(result);
       
+    });
+    // get allusers //
+    app.get('/users',async(req,res)=>{
+      const query={};
+      const users=await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+
+
+    // update a user role
+    app.put('/users/admin/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const options = { upsert: true };
+      const updateDoc={
+        $set:{
+          role:'admin'
+        }
+      }
+      const result=await usersCollection.updateOne(filter,updateDoc,options);
+      res.send(result)
+
+
     })
 
 
+
+
+
+
+    // generate JWT token when user in db //
+    app.get('/jwt',async (req,res)=>{
+      const email=req.query.email;
+      const qurry={email:email};
+      const user=await usersCollection.findOne(qurry);
+      if(user){
+        const token=jwt.sign({email},process.env.ACCESS_TOKEN,{expiresIn:'12h'})
+        return res.send({accessToken:token});
+      }
+  // else jodi user na takhe tahole unothorize dakhabe//
+      res.status(403).send({accessToken:''})
+    })
 
 
 
